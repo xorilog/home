f37478a58debf04a0bcb7c26801f0e3df36b21a4
christophe.boucharlat <christophe.boucharlat@gmail.com>
2021-03-30 16:47:55 +0200

    adding kbfs service to auto start keybase

--- systems/modules/profiles/keybase.nix
+++ systems/modules/profiles/keybase.nix
@@ -12,6 +12,10 @@ in
   };
   config = mkIf cfg.enable {
     services.keybase.enable = true;
+    services.kbfs = {
+      enable = true;
+      enableRedirector = true;
+    };
     environment.systemPackages = with pkgs; [ keybase kbfs keybase-gui ];
   };
 }
