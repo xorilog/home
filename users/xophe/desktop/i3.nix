45395e5748cf0837c0888a12bbc2fc8056c1c5e6
christophe.boucharlat <christophe.boucharlat@gmail.com>
2021-04-16 18:12:55 +0200

    fix gthumb

--- users/xophe/desktop/i3.nix
+++ users/xophe/desktop/i3.nix
@@ -25,7 +25,7 @@ in
   ];
   home.sessionVariables = { WEBKIT_DISABLE_COMPOSITING_MODE = 1; };
   home.packages = with pkgs; [
-    gThumb
+    gthumb
     alacritty
     arandr
     # TODO switch to betterlockscreen
