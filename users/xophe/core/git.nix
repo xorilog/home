cb322f0cd7ebdba6457f37204aa74e8e00ccee62
christophe.boucharlat <christophe.boucharlat@gmail.com>
2023-01-04 11:30:02 +0100

    update(users/xophe/core/git): alias ci to signoff
    Signed-off-by: christophe.boucharlat <christophe.boucharlat@gmail.com>

--- users/xophe/core/git.nix
+++ users/xophe/core/git.nix
@@ -37,7 +37,7 @@
 
     aliases = {
       co = "checkout";
-      ci = "commit";
+      ci = "commit --signoff";
       st = "status";
       br = "branch";
       lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
