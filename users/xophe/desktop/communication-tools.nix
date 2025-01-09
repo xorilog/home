02e72ab2dd9f8b23a508d5d90812a91dfd792fda
christophe.boucharlat <christophe.boucharlat@gmail.com>
2025-01-09 13:41:39 +0100

    fix(zoom-us): fixed to a specific version due to a memory leak in the latest version
    Signed-off-by: christophe.boucharlat <christophe.boucharlat@gmail.com>

--- users/xophe/desktop/communication-tools.nix
+++ users/xophe/desktop/communication-tools.nix
@@ -3,7 +3,14 @@
 {
   home.sessionVariables = { WEBKIT_DISABLE_COMPOSITING_MODE = 1; };
   home.packages = with pkgs; [
-    zoom-us
+    #zoom-us
+    (zoom-us.overrideAttrs {
+      version = "6.2.11.5069";
+      src = pkgs.fetchurl {
+        url = "https://zoom.us/client/6.2.11.5069/zoom_x86_64.pkg.tar.xz";
+        hash = "sha256-k8T/lmfgAFxW1nwEyh61lagrlHP5geT2tA7e5j61+qw=";
+      };
+    })
     signal-desktop
     slack
     discord
