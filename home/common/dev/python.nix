88a93f2fd245a76974e1632ab9a14319cbee5da3
christophe.boucharlat <christophe.boucharlat@gmail.com>
2025-11-19 18:24:12 +0100

    update(python): various libraries
    Signed-off-by: christophe.boucharlat <christophe.boucharlat@gmail.com>

--- home/common/dev/python.nix
+++ home/common/dev/python.nix
@@ -1,8 +1,21 @@
-{ pkgs, ... }:
-
+{ config, pkgs, ... }:
 {
+  #xdg.configFile."python/pythonrc".source = ./python/pythonrc;
   home.packages = with pkgs; [
-    python3
-    pipenv
+    (pkgs.python313.withPackages (
+      p: with p; [
+        tox
+        virtualenv
+        python-lsp-server
+        flake8
+        mccabe
+      ]
+    ))
+    uv
+    ruff
+    # black
   ];
+  #home.sessionVariables = {
+  #  PYTHONSTARTUP = "${config.xdg.configHome}/python/pythonrc";
+  #};
 }
