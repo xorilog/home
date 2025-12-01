af622417c5025714b97afb862cbeae02e5d63e2a
christophe.boucharlat <christophe.boucharlat@gmail.com>
2025-12-01 15:55:21 +0100

    test(xophe-mbp): adding darwin tweaks
    Signed-off-by: christophe.boucharlat <christophe.boucharlat@gmail.com>

--- home/default.nix
+++ home/default.nix
@@ -36,17 +36,21 @@
 
   home = {
     inherit username stateVersion;
-    homeDirectory = "/home/${username}";
+    homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${username}" else "/home/${username}";
   };
 
-  nix.settings = {
-    download-buffer-size = 524288000; # 500 MiB
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    use-xdg-base-directories = true;
-  };
+  nix.settings =
+    if pkgs.stdenv.isDarwin then
+      {
+        download-buffer-size = 524288000; # 500 MiB
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        use-xdg-base-directories = true;
+      }
+    else
+      { };
 
   nixpkgs = {
     overlays = [
